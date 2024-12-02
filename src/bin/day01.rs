7b01d8e9efe82515a3089862f4370ccf3134c47b
Frank Uhlig <uhlig.frank@gmail.com>
2024-12-02 21:48:54 +0100

    add: unintelligent input parsing

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,5 +1,19 @@
 use aoc24::add;
+use std::fs;
 
 fn main() {
     println!("Day 1 {}", add(30, 12));
+    let mut args = std::env::args();
+    args.next().expect("no program name?");
+    let fname = args.next().expect("provide input file name");
+    let data = fs::read_to_string(fname).unwrap();
+
+    let mut left: Vec<i32> = Vec::new();
+    let mut right: Vec<i32> = Vec::new();
+
+    let _ = data.lines().map(|s| {
+        let mut i = s.split(" ");
+        left.push(i.next().unwrap().parse().unwrap());
+        right.push(i.next().unwrap().parse().unwrap());
+    });
 }
